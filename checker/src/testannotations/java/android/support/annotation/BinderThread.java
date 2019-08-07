4dee44139b2d84f8f0bde00b17cc7b92452012f2
aagarwal1012 <aagarwal9782@gmail.com>
2019-08-08 01:25:37 +0530

    ptch: added Android MainThread and BinderThread for testing

---
+++ checker/src/testannotations/java/android/support/annotation/BinderThread.java
@@ -0,0 +1,19 @@
+/**
+ * Upstream version:
+ * https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/annotation/BinderThread.java
+ * This annotation is only included for testing purpose.
+ */
+package android.support.annotation;
+
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.SOURCE;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+@Retention(SOURCE)
+@Target({METHOD, CONSTRUCTOR, TYPE, PARAMETER})
+public @interface BinderThread {}
